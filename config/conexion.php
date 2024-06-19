63e2670e949557baf65dc47eb1d505ed23dd0fd5
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-06-18 20:35:48 -0400

    Subiendo a GitHub4

--- config/conexion.php
+++ config/conexion.php
@@ -9,7 +9,7 @@
                 //localhost
                 /* $conectar = $this->dbh = new PDO("mysql:local=localhost;port=3380;dbname=helppro","root",""); */
                 //Producion
-                $conectar = $this->dbh = new PDO("mysql:host=localhost;dbname=helppro","u359284306_helppro","Banjersing@01");
+                $conectar = $this->dbh = new PDO("mysql:host=localhost;dbname=u359284306_helppro","u359284306_helppro","Banjersing@01");
                 return $conectar;
             } catch (Exception $e) {
                 print "!Error DB!: " . $e->getMessage() . "<br/>";
