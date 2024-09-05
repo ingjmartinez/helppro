63a4dda4c294a45cc4af1556e5ea8083acab3aa0
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-09-05 16:13:54 -0400

    subiendo20

--- controller/usuario.php
+++ controller/usuario.php
@@ -1,6 +1,6 @@
 <?php
     require_once("../config/conexion.php");
-    require_once("../models/usuario.php");
+    require_once("../models/Usuario.php");
     $usuario = new Usuario();
 
     switch($_GET["op"]){
