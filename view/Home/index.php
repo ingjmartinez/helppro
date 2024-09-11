533c9e08048d296f3374b04c12bd9e89f9a32fdb
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-09-11 15:34:52 -0400

    subiendo21

--- view/Home/index.php
+++ view/Home/index.php
@@ -9,76 +9,67 @@ if (isset($_SESSION["usu_id"])) {
 	<title>HelpPro</>::Home</title>
 	</head>
 
-	<body class="with-side-menu">
+    <?php require_once("../MainHeader/header.php");?>
 
-		<?php require_once("../MainHeader/header.php"); ?>
+    <div class="mobile-menu-left-overlay"></div>
+    
+    <?php require_once("../MainNav/nav.php");?>
 
-		<div class="mobile-menu-left-overlay"></div>
-
-		<?php require_once("../MainNav/nav.php"); ?>
-
-		<!-- Contenido -->
-		<div class="page-content">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-xl-12">
-						<div class="row">
-							<div class="col-sm-4">
-								<article class="statistic-box green">
-									<div>
-										<div class="number" id="lbltotal"></div>
-										<div class="caption">
-											<div>Total de Tickets</div>
-										</div>
-									</div>
-								</article>
-							</div>
-							<div class="col-sm-4">
-								<article class="statistic-box yellow">
-									<div>
-										<div class="number" id="lbltotalabierto"></div>
-										<div class="caption">
-											<div>Total de Tickets Abiertos</div>
-										</div>
-									</div>
-								</article>
-							</div>
-							<div class="col-sm-4">
-								<article class="statistic-box red">
-									<div>
-										<div class="number" id="lbltotalcerrado"></div>
-										<div class="caption">
-											<div>Total de Tickets Cerrados</div>
-										</div>
-									</div>
-								</article>
-							</div>
-						</div>
+	<!-- Contenido -->
+	<div class="page-content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="row">
+						<div class="col-sm-4">
+	                        <article class="statistic-box green">
+	                            <div>
+	                                <div class="number" id="lbltotal"></div>
+	                                <div class="caption"><div>Total de Tickets</div></div>
+	                            </div>
+	                        </article>
+	                    </div>
+						<div class="col-sm-4">
+	                        <article class="statistic-box yellow">
+	                            <div>
+	                                <div class="number" id="lbltotalabierto"></div>
+	                                <div class="caption"><div>Total de Tickets Abiertos</div></div>
+	                            </div>
+	                        </article>
+	                    </div>
+						<div class="col-sm-4">
+	                        <article class="statistic-box red">
+	                            <div>
+	                                <div class="number" id="lbltotalcerrado"></div>
+	                                <div class="caption"><div>Total de Tickets Cerrados</div></div>
+	                            </div>
+	                        </article>
+	                    </div>
 					</div>
-					<section class="card">
-						<header class="card-header">
-							Grafico Estadístico
-						</header>
-						<div class="card-block">
-							<div id="divgrafico" style="height: 250px;"></div>
-						</div>
-					</section>
 				</div>
 			</div>
 
-			<!-- Contenido -->
+			<section class="card">
+				<header class="card-header">
+					Grafico Estadístico
+				</header>
+				<div class="card-block">
+					<div id="divgrafico" style="height: 250px;"></div>
+				</div>
+			</section>
+			
+		</div>
+	</div>
+	<!-- Contenido -->
 
-			<?php require_once("../MainJs/js.php"); ?>
+	<?php require_once("../MainJs/js.php");?>
 
-			<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-			<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-			<script type="text/javascript" src="home.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+	<script type="text/javascript" src="home.js"></script>
 
-
-
-	</body>
-
-	</html>
+</body>
+</html>
 <?php
 } else {
 	header("Location: " . Conectar::ruta() . "index.php");
