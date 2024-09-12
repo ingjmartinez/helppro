33b2efcffb34d6d8121337a35fe900af4456b783
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-09-12 17:11:26 -0400

    subiendo23

--- view/ConsultarTicket/index.php
+++ view/ConsultarTicket/index.php
@@ -1,49 +1,50 @@
 <?php
-    date_default_timezone_set('America/Santo_Domingo');
-	error_reporting(E_ALL);
-	ini_set('ignore_repeated_errors', TRUE);
-	ini_set('display_errors', FALSE);
-	ini_set('log_errors', TRUE);
-	ini_set('error_log', 'debug.log');
-	
-	require_once("../../config/conexion.php");
-	if(isset($_SESSION["usu_id"])){
+date_default_timezone_set('America/Santo_Domingo');
+error_reporting(E_ALL);
+ini_set('ignore_repeated_errors', TRUE);
+ini_set('display_errors', FALSE);
+ini_set('log_errors', TRUE);
+ini_set('error_log', 'debug.log');
+
+require_once("../../config/conexion.php");
+if (isset($_SESSION["usu_id"])) {
 ?>
-<!DOCTYPE html>
-<html>
-    <?php require_once("../MainHead/head.php");?>
-    <title>HelpPro</>::Consultar Ticket</title>
-</head>
-<body class="with-side-menu">
+	<!DOCTYPE html>
+	<html>
+	<?php require_once("../MainHead/head.php"); ?>
+	<title>HelpPro</>::Consultar Ticket</title>
+	</head>
 
-    <?php require_once("../MainHeader/header.php");?>
+	<body class="with-side-menu">
 
-	<div class="mobile-menu-left-overlay"></div>
-	
-	<?php require_once("../MainNav/nav.php");?>
+		<?php require_once("../MainHeader/header.php"); ?>
+
+		<div class="mobile-menu-left-overlay"></div>
+
+		<?php require_once("../MainNav/nav.php"); ?>
 
 		<!-- Contenido -->
-	<div class="page-content">
-		<div class="container-fluid">
-			
-			<header class="section-header">
-				<div class="tbl">
-					<div class="tbl-row">
-						<div class="tbl-cell">
-							<h3>Consultar Ticket</h3>
-							<ol class="breadcrumb breadcrumb-simple">
-								<li><a href="#">Home</a></li>
-								<li class="active">Consultar Ticket</li>
-							</ol>
+		<div class="page-content">
+			<div class="container-fluid">
+
+				<header class="section-header">
+					<div class="tbl">
+						<div class="tbl-row">
+							<div class="tbl-cell">
+								<h3>Consultar Ticket</h3>
+								<ol class="breadcrumb breadcrumb-simple">
+									<li><a href="#">Home</a></li>
+									<li class="active">Consultar Ticket</li>
+								</ol>
+							</div>
 						</div>
 					</div>
-				</div>
-			</header>
+				</header>
 
-			<div class="box-typical box-typical-padding">
-				<table id="ticket_data" class="table table-bordered table-striped table-vcenter js-datatable-full">
-					<thead
-						<tr>
+				<div class="box-typical box-typical-padding">
+					<table id="ticket_data" class="table table-bordered table-striped table-vcenter js-datatable-full">
+						<thead
+							<tr>
 							<th style="width: 5%;">Nro. Ticket</th>
 							<th style="width: 10%;">Categoria</th>
 							<th class="d-none d-sm-table-cell" style="width: 15%;">Titulo</th>
@@ -52,33 +53,33 @@
 							<th class="d-none d-sm-table-cell" style="width: 10%;">Fecha Asignacion</th>
 							<th class="d-none d-sm-table-cell" style="width: 10%;">Soporte</th>
 							<th class="text-center" style="width: 5%;"></th>
-						</tr>
-					</thead>
-					<tbody>
+							</tr>
+						</thead>
+						<tbody>
 
-					</tbody>
-				</table>
+						</tbody>
+					</table>
+				</div>
+
 			</div>
-
 		</div>
-	</div>
 
 		<!-- Contenido -->
-		
-
-	<?php require_once("../MainJs/js.php");?>
-
-	<script type="text/javascript" src="consultarticket.js"></script>
-
-	<!-- <script type="text/javascript" src="consultarticket.js"></script> -->
 
 
-	
-</body>
-</html>
+		<?php require_once("../MainJs/js.php"); ?>
+
+		<script type="text/javascript" src="consultarticket.js"></script>
+
+		<!-- <script type="text/javascript" src="consultarticket.js"></script> -->
+
+
+
+	</body>
+
+	</html>
 <?php
- } else {
+} else {
 	header("Location: " . Conectar::ruta() . "index.php");
-
- }
+}
 ?>
