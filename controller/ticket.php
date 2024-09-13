3cd4226b160c7d3c1b97080a5c3c0995ce4f13d6
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-09-12 20:58:14 -0400

    subiendo22

--- controller/ticket.php
+++ controller/ticket.php
@@ -1,8 +1,10 @@
 <?php
-date_default_timezone_set('America/Santo_Domingo');
 require_once("../config/conexion.php");
 require_once("../models/Ticket.php");
 $ticket = new Ticket();
+
+require_once("../models/Usuario.php");
+$usuario = new Usuario();
 
 switch ($_GET["op"]) {
 
@@ -79,6 +81,15 @@ switch ($_GET["op"]) {
                 $sub_array[] = '<span class="label label-pill label-default">Sin Asignar</span>';
             }else{
                 $sub_array[] = date("d/m/Y H:i:s", strtotime($row["fech_asig"]));
+            }
+
+            if($row["usu_asig"]==null){
+                $sub_array[] = '<span class="label label-pill label-warning">Sin Asignar</span>';
+            }else{
+                $datos1=$usuario->get_usuario_x_id($row["usu_asig"]);
+                foreach($datos1 as $row1){
+                    $sub_array[] = '<span class="label label-pill label-success">'. $row1["usu_nom"].'</span>';
+                }
             }
 
             $sub_array[] = '<button type="button" onClick="ver('.$row["tick_id"].');"  id="'.$row["tick_id"].'" class="btn btn-inline btn-primary btn-sm ladda-button"><i class="fa fa-eye"></i></button>';
