03885bcb91f6579494c775fe12773a0f7b5ea844
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-09-17 12:53:44 -0400

    subiendo27.1

--- view/NuevoTicket/index.php
+++ view/NuevoTicket/index.php
@@ -45,23 +45,33 @@
 
 					<input type="hidden" id="usu_id" name="usu_id" value="<?php echo $_SESSION["usu_id"] ?>">
 
+					<div class="col-lg-12">
+							<fieldset class="form-group">
+								<label class="form-label semibold" for="tick_titulo">Titulo</label>
+								<input type="text" class="form-control" id="tick_titulo" name="tick_titulo" placeholder="Ingrese Titulo">
+							</fieldset>
+						</div>
+
 						<div class="col-lg-6">
 							<fieldset class="form-group">
 								<label class="form-label semibold" for="exampleInput">Categoria</label>
 								<select id="cat_id" name="cat_id" class="form-control">
+
 								</select>
 							</fieldset>
 						</div>
+
 						<div class="col-lg-6">
 							<fieldset class="form-group">
-								<label class="form-label semibold" for="tick_titulo">Titulo</label>
-								<input type="Text" class="form-control" id="tick_titulo" name="tick_titulo" placeholder="Ingrese Titulo">
+								<label class="form-label semibold" for="exampleInput">Documentos Adicionales</label>
+								<input type="file" name="fileElem" id="fileElem" class="form-control" multiple>
 							</fieldset>
 						</div>
+
 						<div class="col-lg-12">
 							<fieldset class="form-group">
 								<label class="form-label semibold" for="tick_descrip">Descripción</label>
-								<div class="summernote-theme-1" >
+								<div class="summernote-theme-1">
 									<textarea id="tick_descrip" name="tick_descrip" class="summernote" name="name"></textarea>
 								</div>
 							</fieldset>
