df5c9816b5f3f1d18f6a28f49d3d9bc4659d643e
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-09-17 22:55:05 -0400

    subiendo27.6

--- view/Home/index.php
+++ view/Home/index.php
@@ -4,75 +4,79 @@ if (isset($_SESSION["usu_id"])) {
 ?>
 	<!DOCTYPE html>
 	<html>
-	<?php require_once("../MainHead/head.php"); ?>
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-	<title>HelpPro</>::Home</title>
+
+	<head>
+		<?php require_once("../MainHead/head.php"); ?>
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+		<title>HelpPro</>::Home</title>
 	</head>
 
-	<?php require_once("../MainHeader/header.php"); ?>
+	<body class="with-side-menu">
 
-	<div class="mobile-menu-left-overlay"></div>
+		<?php require_once("../MainHeader/header.php"); ?>
 
-	<?php require_once("../MainNav/nav.php"); ?>
+		<div class="mobile-menu-left-overlay"></div>
 
-	<!-- Contenido -->
-	<div class="page-content">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-xl-12">
-					<div class="row">
-						<div class="col-sm-4">
-							<article class="statistic-box green">
-								<div>
-									<div class="number" id="lbltotal"></div>
-									<div class="caption">
-										<div>Total de Tickets</div>
+		<?php require_once("../MainNav/nav.php"); ?>
+
+		<!-- Contenido -->
+		<div class="page-content">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xl-12">
+						<div class="row">
+							<div class="col-sm-4">
+								<article class="statistic-box green">
+									<div>
+										<div class="number" id="lbltotal"></div>
+										<div class="caption">
+											<div>Total de Tickets</div>
+										</div>
 									</div>
-								</div>
-							</article>
-						</div>
-						<div class="col-sm-4">
-							<article class="statistic-box yellow">
-								<div>
-									<div class="number" id="lbltotalabierto"></div>
-									<div class="caption">
-										<div>Total de Tickets Abiertos</div>
+								</article>
+							</div>
+							<div class="col-sm-4">
+								<article class="statistic-box yellow">
+									<div>
+										<div class="number" id="lbltotalabierto"></div>
+										<div class="caption">
+											<div>Total de Tickets Abiertos</div>
+										</div>
 									</div>
-								</div>
-							</article>
-						</div>
-						<div class="col-sm-4">
-							<article class="statistic-box red">
-								<div>
-									<div class="number" id="lbltotalcerrado"></div>
-									<div class="caption">
-										<div>Total de Tickets Cerrados</div>
+								</article>
+							</div>
+							<div class="col-sm-4">
+								<article class="statistic-box red">
+									<div>
+										<div class="number" id="lbltotalcerrado"></div>
+										<div class="caption">
+											<div>Total de Tickets Cerrados</div>
+										</div>
 									</div>
-								</div>
-							</article>
+								</article>
+							</div>
 						</div>
 					</div>
 				</div>
+
+				<section class="card">
+					<header class="card-header">
+						Grafico Estadístico
+					</header>
+					<div class="card-block">
+						<div id="divgrafico" style="height: 250px;"></div>
+					</div>
+				</section>
+
 			</div>
-
-			<section class="card">
-				<header class="card-header">
-					Grafico Estadístico
-				</header>
-				<div class="card-block">
-					<div id="divgrafico" style="height: 250px;"></div>
-				</div>
-			</section>
-
 		</div>
-	</div>
-	<!-- Contenido -->
+		<!-- Contenido -->
 
-	<?php require_once("../MainJs/js.php"); ?>
+		<?php require_once("../MainJs/js.php"); ?>
 
-	<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-	<script type="text/javascript" src="home.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+		<script type="text/javascript" src="home.js"></script>
 
 	</body>
 
