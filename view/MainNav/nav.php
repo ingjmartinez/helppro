658bc1ff1b4ca0efc9af5dc5085a17f611b80b7c
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-10-22 22:59:07 -0400

    subiendo34.7

--- view/MainNav/nav.php
+++ view/MainNav/nav.php
@@ -40,6 +40,12 @@ if ($_SESSION["rol_id"] == 1) {
                         </a>
                     </li>
                 </ul>
+                    <li class="blue-dirty">
+                        <a href="..\Faltantes\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Reportes BI</span>
+                        </a>
+                    </li>
             </li>
         </ul>
     </nav>
