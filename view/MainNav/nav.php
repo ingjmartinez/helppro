bb6d61f608613d6b05e31ed3446a3f43ee3670d7
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-10-22 20:51:48 -0400

    subiendo34.2

--- view/MainNav/nav.php
+++ view/MainNav/nav.php
@@ -1,60 +1,70 @@
 <?php
-    if ($_SESSION["rol_id"]==1){
-        ?>
-            <nav class="side-menu">
-                <ul class="side-menu-list">
-                    <li class="blue-dirty">
-                        <a href="..\Home\">
-                            <span class="glyphicon glyphicon-th"></span>
-                            <span class="lbl">Inicio</span>
-                        </a>
-                    </li>
-                    <li class="blue-dirty">
-                        <a href="..\NuevoTicket\">
-                            <span class="glyphicon glyphicon-th"></span>
-                            <span class="lbl">Nuevo Ticket</span>
-                        </a>
-                    </li>
+if ($_SESSION["rol_id"] == 1) {
+?>
+    <nav class="side-menu">
+        <ul class="side-menu-list">
+            <li class="blue-dirty">
+                <a href="..\Home\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Inicio</span>
+                </a>
+            </li>
+            <li class="blue-dirty">
+                <a href="..\NuevoTicket\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Nuevo Ticket</span>
+                </a>
+            </li>
 
-                    <li class="blue-dirty">
-                        <a href="..\ConsultarTicket\">
-                            <span class="glyphicon glyphicon-th"></span>
-                            <span class="lbl">Consultar Ticket</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        <?php
-    }else{
-        ?>
-            <nav class="side-menu">
-                <ul class="side-menu-list">
-                    <li class="blue-dirty">
-                        <a href="..\Home\">
-                            <span class="glyphicon glyphicon-th"></span>
-                            <span class="lbl">Inicio</span>
-                        </a>
-                    </li>
-                    <li class="blue-dirty">
-                        <a href="..\MntUsuario\">
-                            <span class="glyphicon glyphicon-th"></span>
-                            <span class="lbl">Mantenimiento Usuario</span>
-                        </a>
-                    </li>
-               <!--      <li class="blue-dirty">
+            <li class="blue-dirty">
+                <a href="..\ConsultarTicket\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Consultar Ticket</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+<?php
+} else {
+?>
+    <nav class="side-menu">
+        <ul class="side-menu-list">
+            <li class="blue-dirty">
+                <a href="..\Home\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Inicio</span>
+                </a>
+            </li>
+            <li class="blue-dirty">
+                <a href="..\MntUsuario\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Mantenimiento Usuario</span>
+                </a>
+            </li>
+            <!--      <li class="blue-dirty">
                         <a href="..\MntUsuario\">
                             <span class="glyphicon glyphicon-th"></span>
                             <span class="lbl">Mantenimiento Usuario</span>
                         </a>
                     </li> -->
-                    <li class="blue-dirty">
-                        <a href="..\ConsultarTicket\">
-                            <span class="glyphicon glyphicon-th"></span>
-                            <span class="lbl">Consultar Ticket</span>
-                        </a>
-                    </li>
+            <li class="blue-dirty">
+                <a href="..\ConsultarTicket\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Consultar Ticket</span>
+                </a>
+            </li>
+            <li class="grey with-sub">
+                <span>
+                    <i class="font-icon font-icon-dashboard"></i>
+                    <span class="lbl">Dashboard</span>
+                </span>
+                <ul>
+                    <li><a href="index.html"><span class="lbl">Default</span><span class="label label-custom label-pill label-danger">new</span></a></li>
+                    <li><a href="dashboard-top-menu.html"><span class="lbl">Top menu</span></a></li>
                 </ul>
-            </nav>
-        <?php
-    }
+            </li>
+        </ul>
+    </nav>
+<?php
+}
 ?>
