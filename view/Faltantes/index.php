ec6a1b83cd1d73d97be431600409cb0ca2f49ede
ingjmartinez <162402896+ingjmartinez@users.noreply.github.com>
2024-10-22 21:53:08 -0400

    subiendo34.6

--- view/Faltantes/index.php
+++ view/Faltantes/index.php
@@ -39,15 +39,6 @@ if (isset($_SESSION["usu_id"])) {
                 </div>
             </div>
         </div>
-
-        <!-- Contenido -->
-
-        <?php require_once("modalasignar.php"); ?>
-        <?php require_once("../MainJs/js.php"); ?>
-
-        <script type="text/javascript" src="consultarticket.js"></script>
-
-        <!-- <script type="text/javascript" src="consultarticket.js"></script> -->
     </body>
 
     </html>
